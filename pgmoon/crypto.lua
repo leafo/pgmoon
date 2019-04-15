dff601a3cf44b1e2fe064c3f7e5caca8cc5439a9
leaf corcoran <leafot@gmail.com>
2019-04-15 16:27:23 -0700

    use luaossl for md5 before lua crypto, add better error #79

--- pgmoon/crypto.lua
+++ pgmoon/crypto.lua
@@ -3,10 +3,31 @@ if ngx then
     md5 = ngx.md5
   }
 end
-local crypto = require("crypto")
 local md5
-md5 = function(str)
-  return crypto.digest("md5", str)
+pcall(function()
+  local digest = require("openssl.digest")
+  local hex_char
+  hex_char = function(c)
+    return string.format("%02x", string.byte(c))
+  end
+  local hex
+  hex = function(str)
+    return (str:gsub(".", hex_char))
+  end
+  md5 = function(str)
+    return hex(digest.new("md5"):final(str))
+  end
+end)
+if not (md5) then
+  pcall(function()
+    local crypto = require("crypto")
+    md5 = function(str)
+      return crypto.digest("md5", str)
+    end
+  end)
+end
+if not (md5) then
+  error("Either luaossl (recommended) or LuaCrypto is required to calculate md5")
 end
 return {
   md5 = md5
