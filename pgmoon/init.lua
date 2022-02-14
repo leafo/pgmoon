cd9f0922672a1d2cea1e5afb6433e5338cbbe400
Wangchong Zhou <fffonion@gmail.com>
2022-02-14 18:15:32 +0800

    fix(init) use lua-resty-openssl instead of luaossl to let it run with BoringSSL
    BoringSSL changed the size type of PBKDF2 functions, thus not binary
    compatible with OpenSSL library. This PR further removes dependency of
    luaossl in other places to ensure there're no other incompatible with
    BoringSSL.

--- pgmoon/init.lua
+++ pgmoon/init.lua
@@ -288,7 +288,7 @@ do
     scram_sha_256_auth = function(self, msg)
       assert(self.password, "missing password, required for connect")
 
-      local openssl_rand = require("openssl.rand")
+      local openssl_rand = require("resty.openssl.rand")
 
       -- '18' is the number set by postgres on the server side
       local rand_bytes, err = openssl_rand.bytes(18)
@@ -355,7 +355,7 @@ do
             signature = "sha256"
           end
 
-          local openssl_x509 = require("openssl.x509").new(pem, "PEM")
+          local openssl_x509 = require("resty.openssl.x509").new(pem, "PEM")
 
           local openssl_x509_digest, err = openssl_x509:digest(signature, "s")
 
@@ -404,7 +404,7 @@ do
       local client_final_message_without_proof = channel_binding .. "," .. nonce
 
       local function hmac(key, str)
-        local openssl_hmac = require("openssl.hmac")
+        local openssl_hmac = require("resty.openssl.hmac")
         local hmac, err = openssl_hmac.new(key, "sha256")
 
         if not (hmac) then
@@ -423,7 +423,7 @@ do
       end
 
       local function h(str)
-        local openssl_digest, err = require("openssl.digest").new("sha256")
+        local openssl_digest, err = require("resty.openssl.digest").new("sha256")
 
         if not (openssl_digest) then
           return nil, tostring(err)
@@ -458,15 +458,15 @@ do
       end
 
       local function hi(str, salt, i)
-        local openssl_kdf = require("openssl.kdf")
+        local openssl_kdf = require("resty.openssl.kdf")
 
         salt = ngx.decode_base64(salt)
 
         local key, err = openssl_kdf.derive({
-          type = "PBKDF2",
+          type = openssl_kdf.PBKDF2,
           md = "sha256",
           salt = salt,
-          iter = i,
+          pbkdf2_iter = i,
           pass = str,
           outlen = 32 -- our H() produces a 32 byte hash value (SHA-256)
         })
